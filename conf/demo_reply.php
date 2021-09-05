6182bd6dbf240a970b61071ebda3e7ead4c9b6e9
zhuanke8 <64455988+zhuanke8@users.noreply.github.com>
2021-09-05 16:53:09 +0800

    Update demo_reply.php

--- conf/demo_reply.php
+++ conf/demo_reply.php
@@ -2,7 +2,7 @@ return reply(
      [
           `早上好`=>`good morning`
           `壁纸` => `https://acg.toubiec.cn/random.php`
-          `你好`=> `好个锤子`
+          `你好`=> `你也好`
           `姐姐` => `http://3650000.xyz/random/?mode=66`
           `妹妹` => `https://laosepi.org/pic.php`
           `02` => `http://api.btstu.cn/sjbz/?lx=dongman`
@@ -20,15 +20,13 @@ return reply(
           `语录` => `https://api.ixiaowai.cn/ylapi/index.php`
           `舔狗` => `https://api.ixiaowai.cn/tgrj/index.php`
           `.*一言.*`=>`https://api.ixiaowai.cn/ylapi/index.php`
-         `更新` => `发送扫码或进入网站绑定QQ`
-         `公告` => `发送CK绑定QQ后输入"查询"`
-         `密码` => `你猜`
+         `扫码` => `发送pt_key=;pt_pin=; 这种格式给我，记得要;这个符号，提示很棒，即为成功，不要一直给我发消息，一分钟超过3条就会被自动拉黑。如果发送了CK但是我没回复你就是CK失效了。重新提取CK发给我。`
          `教程` => `手机端Alook浏览器获取Cookie教程“https://docs.qq.com/doc/DTFhQUGlhSVlZckRV” PC端获取Cookie教程“https://shimo.im/docs/YcdghjYdKY9yPDK6”`
          `菜单` => `
                 小滴滴菜单
        ———————————     
             教程  丨  查询       
-            公告  丨  扫码
+            状态  丨  扫码
        ———————————             
                 娱乐菜单
        ———————————    
@@ -37,7 +35,7 @@ return reply(
             妹妹  丨 美女       
             风景  丨 壁纸       
             你好  丨 姐姐       
-            状态  丨 许愿
+            打卡  丨 许愿
             骚话  丨 一言
        二次元图丨 网易热评 
        动漫头像丨 舔狗日记       
