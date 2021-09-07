c55bbb110a1340aeaf1e6ff92599cf6e7890776c
zhuanke8 <64455988+zhuanke8@users.noreply.github.com>
2021-09-08 02:21:10 +0800

    Update demo_reply.php

--- conf/demo_reply.php
+++ conf/demo_reply.php
@@ -1,14 +1,16 @@
 return reply(
      [
           `早上好`=>`good morning`
-          `壁纸` => `https://acg.toubiec.cn/random.php`
-          `你好`=> `你也好`
+          `干饭了`=>`除了吃你还会什么？`
+          `晚安`=>`早点睡 猪逼`
+          `壁纸` => `https://api.ixiaowai.cn/gqapi/gqapi.php`
+          `你好`=> `好个牛子`
           `姐姐` => `http://3650000.xyz/random/?mode=66`
           `妹妹` => `https://laosepi.org/pic.php`
           `02` => `http://api.btstu.cn/sjbz/?lx=dongman`
           `风景` => `https://laosepi.org/gqpic.php`
           `美女` => `https://api.lyiqk.cn/purelady`
-          `二次元图` => `https://acg.toubiec.cn/random.php`
+          `二次元图` => `https://api.ixiaowai.cn/api/api.php`
           `网易热评` => `https://api.uomg.com/api/comments.163?format=json`
           `动漫头像` => `http://shengapi.cn/api/image/acgtx.php`
           `舔狗日记` => `http://shengapi.cn/api/tgrj.php`
@@ -21,12 +23,13 @@ return reply(
           `舔狗` => `https://api.ixiaowai.cn/tgrj/index.php`
           `.*一言.*`=>`https://api.ixiaowai.cn/ylapi/index.php`
          `扫码` => `发送pt_key=;pt_pin=; 这种格式给我，记得要;这个符号，提示很棒/重复/更新，即为成功。提示无效请重新抓取，到期失效会发消息提醒。`
+         `公告` => `发送CK绑定QQ后输入"查询"`
          `教程` => `手机端Alook浏览器获取Cookie教程“https://docs.qq.com/doc/DTFhQUGlhSVlZckRV” PC端获取Cookie教程“https://shimo.im/docs/YcdghjYdKY9yPDK6”`
          `菜单` => `
                 小滴滴菜单
        ———————————     
             教程  丨  查询       
-            状态  丨  扫码
+            公告  丨  扫码
        ———————————             
                 娱乐菜单
        ———————————    
